a098c490ef302a8a398665a56c6a661fdcf5ef95
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-08-09 14:14:39 +0200

    upload

---
+++ index.js
@@ -0,0 +1,29 @@
+require("express-async-errors");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const connection = require("./db");
+const cors = require("cors");
+const port = 8080;
+
+(async function db() {
+  await connection();
+})();
+
+app.use(cors());
+app.use(express.json());
+
+// API routes
+app.use("/api/v1", require("./routes/index.route"));
+
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.status(500).json({ error: error.message });
+});
+
+app.listen(port, () => {
+  console.log("Listening to Port ", port);
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Secure Password Reset 
+
+This is a password reset sample project
+
+Workflow
+
+![Sample](reset-password.png "Workflow")
+
+Follow the tutorial [here]("") to learn the implementation
+
+
